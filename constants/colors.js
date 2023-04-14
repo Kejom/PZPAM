b24d63b879ffde6d71d81c2f215bae2302830c09
ArturZ <artur.zorawski@microsoft.wsei.edu.pl>
2023-04-14 21:30:31 +0200

    added constants folder and GlobalColors

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+export const GlobalColors = {
+    primaryLight: '#8BC34A',
+    primary: '#4CAF50',
+    primaryDark: '#689F38',
+    secondaryLight: '#FFC107',
+    secondary: '#FF9800',
+    secondaryDark: '#FF5722',
+    greyLight: '#F5F5F5',
+    greyDark: '#9E9E9E',
+    lemon: '#CDDC39',
+    accent: '#FFEB3B'
+}
