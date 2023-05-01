2fdceff66d074312070c7e347ee1b5cc4129586d
ArturZ <artur.zorawski@microsoft.wsei.edu.pl>
2023-05-02 00:13:06 +0200

    test changes

--- tests/components/shared/Accordian.test.js
+++ tests/components/shared/Accordian.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render} from '@testing-library/react-native';
 import Accordian from '../../../components/shared/Accordian';
 import { Text } from 'react-native';
 
 describe('Accordian', () => {
-    it('should render correctly', () => {
-      const {getByText } = render(
-        <Accordian title="Test Title" style={{}}><Text>Test Content</Text></Accordian>
-      );
-
-      const title = getByText('Test Title');
-      expect(title).toBeDefined();
-
-    });
+    it('renders correctly', () => {
+        const { toJSON } = render(
+          <Accordian title="Title">
+            <Text>Content</Text>
+          </Accordian>
+        );
+        expect(toJSON()).toMatchSnapshot();
+      });
   });
